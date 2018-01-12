0727aa23c06ea98ec62d4799a67a794ef2174abe
Faraz Khan <farazcsk@gmail.com>
2018-01-12 11:59:24 +0000

    Add basic routing and Home screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './screens/Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Lingo</h1>
+        <Router>
+          <Route exact path="/" component={ Home }/>
+        </Router>
       </div>
     );
   }
