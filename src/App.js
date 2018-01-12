bbf5835e53109973a825eadb898f6e243f8bc4a8
Faraz Khan <farazcsk@gmail.com>
2018-01-12 12:32:28 +0000

    Add eslint configuation from Bloom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './screens/Home/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Lingo</h1>
-        <Router>
-          <Route exact path="/" component={ Home }/>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>Lingo</h1>
+    <Router>
+      <Route exact path="/" component={ Home } />
+    </Router>
+  </div>
+);
 
 export default App;
