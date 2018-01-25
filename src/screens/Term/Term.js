5f2d2fafd2afb0c9fc185d075339e48471ab5611
Faraz Khan <farazcsk@gmail.com>
2018-01-25 11:07:11 +0000

    AH-363 Update Term screen styles

--- src/screens/Term/Term.js
+++ src/screens/Term/Term.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
-const Term = ({ match }) => (
-  <div>
-    <h2>{ match.params.term }</h2>
-  </div>
-);
+import Badge from '@appearhere/bloom/components/Badge/Badge';
+
+import terms from '../../terms';
+
+import sharedCss from '../../shared.css';
+import css from './Term.css';
+
+const Term = ({ match }) => {
+  const term = terms.find(
+    t => (t.name === match.params.term) && (t.department === match.params.department)
+  );
+
+  return (
+    <div className={ cx(sharedCss.container, css.root) }>
+      <div className={ css.top }>
+        <h1 className={ css.h1 }>{ term.name }</h1>
+        <Badge className={ css.badge }>{ term.department }</Badge>
+        <div className={ css.definition }>{ term.definition }</div>
+      </div>
+      <div className={ css.bottom }>
+        <div>
+          <h2 className={ css.h2 }>When</h2>
+          <div className={ css.body }>{ term.when }</div>
+        </div>
+        <div>
+          <h2 className={ css.h2 }>Where</h2>
+          <div className={ css.body }>{ term.where }</div>
+        </div>
+        <div>
+          <h2 className={ css.h2 }>How</h2>
+          <h3 className={ css.h3 }>Explain</h3>
+          <div className={ css.body }>{ term.explain }</div>
+          <h3 className={ css.h3 }>Technical</h3>
+          <div className={ css.body }>{ term.technical }</div>
+        </div>
+        <div>
+          <h2 className={ css.h2 }>Expected Boundary</h2>
+          <div className={ css.body }>{ term.expected }</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Term.propTypes = {
   match: PropTypes.shape({}).isRequired,
