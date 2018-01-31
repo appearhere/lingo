0aa6f3c4067d2f41206298a032fb90cce7cc6823
Faraz Khan <farazcsk@gmail.com>
2018-01-31 14:01:53 +0000

    AH-424 Group terms by department on Home screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Fuse from 'fuse.js';
 import qs from 'query-string';
+import flow from 'lodash/fp/flow';
+import sortBy from 'lodash/fp/sortBy';
+import groupBy from 'lodash/fp/groupBy';
+import map from 'lodash/map';
 
 import TermList from '../../components/TermList/TermList';
 
@@ -9,7 +13,7 @@ import terms from '../../terms';
 
 import sharedCss from '../../shared.css';
 
-const options = {
+const fuse = new Fuse(terms, {
   shouldSort: true,
   threshold: 0.1,
   location: 0,
@@ -17,30 +21,23 @@ const options = {
   maxPatternLength: 32,
   minMatchCharLength: 1,
   keys: ['name', 'definition'],
-};
+});
 
-const fuse = new Fuse(terms, options);
+const formatResults = flow(sortBy('name'), groupBy('department'));
 
 const Home = ({ match, location }) => {
   const { q: query } = qs.parse(location.search);
   const currentDepartment = match.params.department;
   const searchResults = query ? fuse.search(query) : terms;
-
-  const filteredTerms = currentDepartment ? searchResults.filter(
-    term => term.department === currentDepartment
-  ) : searchResults;
-
-  const sortedTerms = filteredTerms.sort((a, b) => {
-    if (a.name < b.name) return -1;
-
-    if (a.name > b.name) return 1;
-
-    return 0;
-  });
+  const results = formatResults(searchResults);
 
   return (
     <div className={ sharedCss.container }>
-      <TermList terms={ sortedTerms } />
+      { currentDepartment
+        ? <TermList department={ currentDepartment } terms={ results[currentDepartment] } />
+        : map(results, (groupedTerms, department) => (
+          <TermList key={ department } department={ department } terms={ groupedTerms } />
+      )) }
     </div>
   );
 };
