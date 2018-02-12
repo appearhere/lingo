65a46a48acd4e77c01be4a1e5e4006b3ff86fee9
Faraz Khan <farazcsk@gmail.com>
2018-02-12 14:55:36 +0000

    AH-430 Add ScrollToTop component

---
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+
+class ScrollToTop extends Component {
+  static propTypes = {
+    location: PropTypes.shape({ }),
+    children: PropTypes.node,
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      // eslint-disable-next-line
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+export default withRouter(ScrollToTop);
